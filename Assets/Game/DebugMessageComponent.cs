2ebd3d8b18f0b7b770d7020e37a48aa89c123b7a
furyohfury <139155361+furyohfury@users.noreply.github.com>
2024-08-12 22:04:28 +0500

    chore/added entitas

---
+++ Assets/Game/DebugMessageComponent.cs
@@ -0,0 +1,6 @@
+//using Entitas;
+
+//public class DebugMessageComponent : IComponent
+//{
+//    public string message;
+//}
---
+++ Assets/Game/GameEngine/Scripts/Systems/DebugMessageSystem.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using Entitas;
+using UnityEngine;
+
+public class DebugMessageSystem : ReactiveSystem<GameEntity>
+{
+    public DebugMessageSystem(Contexts contexts) : base(contexts.game)
+    {
+    }
+
+    protected override ICollector<GameEntity> GetTrigger(IContext<GameEntity> context)
+    {
+        // we only care about entities with DebugMessageComponent 
+        return context.CreateCollector(GameMatcher.DebugMessage);
+    }
+
+    protected override bool Filter(GameEntity entity)
+    {
+        // good practice to perform a final check in case 
+        // the entity has been altered in a different system.
+        return entity.hasDebugMessage;
+    }
+
+    protected override void Execute(List<GameEntity> entities)
+    {
+        // this is the list of entities that meet our conditions
+        foreach (var e in entities)
+        {
+            // we can safely access their DebugMessage component
+            // then grab the string data and print it
+            Debug.Log(e.debugMessage.message);
+        }
+    }
+
+public class HelloWorldSystem : IInitializeSystem
+{
+    // always handy to keep a reference to the context 
+    // we're going to be interacting with it
+    readonly GameContext _context;
+
+    public HelloWorldSystem(Contexts contexts)
+    {
+        // get the context from the constructor
+        _context = contexts.game;
+    }
+
+    public void Initialize()
+    {
+        // create an entity and give it a DebugMessageComponent with
+        // the text "Hello World!" as its data
+        _context.CreateEntity().AddDebugMessage("Hello World!");
+    }
+}
+}
