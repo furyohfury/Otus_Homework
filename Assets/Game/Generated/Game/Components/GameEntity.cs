d64eb432256553ea25b89ebe3e06d8efb5e89bdc
furyohfury <139155361+furyohfury@users.noreply.github.com>
2024-08-23 23:04:46 +0500

    chore/generated smth

--- Assets/Game/Generated/Game/Components/GameEntity.cs
+++ Assets/Game/Generated/Game/Components/GameEntity.cs
@@ -38,19 +38,3 @@ public partial class GameEntity {
 //     the code is regenerated.
 // </auto-generated>
 //------------------------------------------------------------------------------
-public sealed partial class GameMatcher {
-
-    static Entitas.IMatcher<GameEntity> _matcherTeam;
-
-    public static Entitas.IMatcher<GameEntity> Team {
-        get {
-            if (_matcherTeam == null) {
-                var matcher = (Entitas.Matcher<GameEntity>)Entitas.Matcher<GameEntity>.AllOf(GameComponentsLookup.Team);
-                matcher.componentNames = GameComponentsLookup.componentNames;
-                _matcherTeam = matcher;
-            }
-
-            return _matcherTeam;
-        }
-    }
-}
---
+++ Assets/Game/Generated/Game/Components/GameMatcher.cs
@@ -0,0 +1,16 @@
+﻿public sealed partial class GameMatcher {
+
+	static Entitas.IMatcher<GameEntity> _matcherTeam;
+
+	public static Entitas.IMatcher<GameEntity> Team {
+		get {
+			if (_matcherTeam == null) {
+				var matcher = (Entitas.Matcher<GameEntity>)Entitas.Matcher<GameEntity>.AllOf(GameComponentsLookup.Team);
+				matcher.componentNames = GameComponentsLookup.componentNames;
+				_matcherTeam = matcher;
+			}
+
+			return _matcherTeam;
+		}
+	}
+}
