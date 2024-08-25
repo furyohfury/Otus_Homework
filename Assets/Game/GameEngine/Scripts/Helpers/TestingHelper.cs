f4a9b401bd4e514c2d093ce72659425116cd6452
furyohfury <139155361+furyohfury@users.noreply.github.com>
2024-08-25 23:53:58 +0500

    chore/new prefabs

---
+++ Assets/Game/GameEngine/Scripts/Helpers/TestingHelper.cs
@@ -0,0 +1,23 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class TestingHelper : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject _prefab;
+
+	[Button]
+	public void ChangeColor(Material material)
+	{
+		foreach (var mesh in _prefab.GetComponentsInChildren<MeshRenderer>())
+		{
+			mesh.material = material;
+		}
+		
+		foreach (var mesh in _prefab.GetComponentsInChildren<SkinnedMeshRenderer>())
+		{
+			mesh.material = material;
+		}
+	}
+
+}
