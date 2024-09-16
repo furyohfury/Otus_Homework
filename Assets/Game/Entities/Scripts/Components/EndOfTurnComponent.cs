10584d9f73069679fa42f3bf44b8344987384200
furyohfury <139155361+furyohfury@users.noreply.github.com>
2024-09-16 17:07:10 +0500

    chore/throwprojectile task in EoTComponent

--- Assets/Game/Entities/Scripts/Components/EndOfTurnComponent.cs
+++ Assets/Game/Entities/Scripts/Components/EndOfTurnComponent.cs
@@ -58,9 +58,38 @@ namespace Entities
 	
 		protected override void OnHandleEvent(DamageRandomEnemyEvent evt)
 		{
-			// _visualPipeline.AddTask(new );
+			// _visualPipeline.AddTask(new ThrowProjectileVisualTask(projectile));
 			Debug.Log("DamageRandomEnemyVisualHandler");
 			
+		}
+	}
+
+	public class ThrowProjectileVisualTask : EventTask
+	{
+		private readonly Entity _source;
+		private readonly Entity _target;
+		private GameObject _projectile;
+		private Transform _worldTransform;
+
+
+		public ThrowProjectileVisualTask(Entity source, Entity target, GameObject projectile, Transform worldTransform)
+		{
+			_source = source;
+			_target = target;
+			_projectile = projectile;
+			_worldTransform = worldTransform;
+		}
+
+		protected override void OnRun()
+		{
+			Debug.Log("ThrowProjectileVisualTask OnRun");
+			var sourceHeroViewComponent = _source.GetData<HeroViewComponent>();
+			var targetHeroViewComponent = _target.GetData<HeroViewComponent>();
+			var startPos = sourceHeroViewComponent.Container.position;
+			var endPos = targetHeroViewComponent.Container.position;
+			
+			projectile.DOMove(startPos, endPos, 1f)
+                .OnComplete(Finish);
 		}
 	}
 }
