1b890e6bd0df469de2857db8f6a6a91abbd06e68
furyohfury <139155361+furyohfury@users.noreply.github.com>
2024-09-17 15:47:34 +0500

    feat/add low health sound

--- Assets/Game/EventBus/Handlers/Visual/DealDamageVisualHandler.cs
+++ Assets/Game/EventBus/Handlers/Visual/DealDamageVisualHandler.cs
@@ -8,13 +8,15 @@ namespace EventBus
     {
         private readonly VisualPipeline _visualPipeline;
         private readonly ParticleSystem _particleSystem;
+         private AudioPlayer _audioPlayer; // TODO think in which task sound should be
 
         [Inject]
-        public DealDamageVisualHandler(EventBus eventBus, VisualPipeline visualPipeline, ParticleSystem particleSystem) : base(eventBus)
+        public DealDamageVisualHandler(EventBus eventBus, VisualPipeline visualPipeline, ParticleSystem particleSystem, AudioPlayer audioPlayer) : base(eventBus)
         {
             _visualPipeline = visualPipeline;
             _particleSystem = particleSystem;
-        }
+            _audioPlayer = audioPlayer;
+        }   
 
         protected override void OnHandleEvent(DealDamageEvent evt)
         {    
@@ -23,7 +25,7 @@ namespace EventBus
             var health = statsComponent.CurrentHealth;
             // TODO delete if wont fuck with attack animation callback
             _visualPipeline.AddTask(new DealDamageVisualTask(evt.Target, damage, health, _particleSystem)); 
-            _visualPipeline.AddTask(new ChangeStatsVisualTask(evt.Target));
+            _visualPipeline.AddTask(new ChangeStatsVisualTask(evt.Target, _audioPlayer));
         }
     }
 }
--- Assets/Game/Pipeline/Tasks/Visual/ChangeStatsVisualTask.cs
+++ Assets/Game/Pipeline/Tasks/Visual/ChangeStatsVisualTask.cs
@@ -6,10 +6,12 @@ namespace EventBus
    public class ChangeStatsVisualTask : EventTask
 	{
 		private readonly Entity _target;
+		private AudioPlayer _audioPlayer;
 
-		public ChangeStatsVisualTask(Entity target)
+		public ChangeStatsVisualTask(Entity target, AudioPlayer audioPlayer)
 		{
 			_target = target;
+			_audioPlayer = audioPlayer;
 		}
 
 		protected override void OnRun()
@@ -18,6 +20,15 @@ namespace EventBus
 			var stats = _target.GetData<StatsComponent>();
 			var targetHeroViewComponent = _target.GetData<HeroViewComponent>();
 			targetHeroViewComponent.HeroView.SetStats($"{stats.Damage}/{stats.CurrentHealth}");
+
+			if (stats.CurrentHP <= stats.MaxHP * 0.2f && currentHero.TryGetData(out HeroSoundComponent heroSoundComponent))
+			{
+				var clips = heroSoundComponent.LowHealthClips;
+				if (clips == null) continue;
+				
+				var randomIndex = Random.Range(0, clips.Length);
+				_audioPlayer.PlaySound(clips[randomIndex]);
+			}	
 			Finish();
 		}
 	} 
