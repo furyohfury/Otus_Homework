83b52c39c03fcbdff70c32b128d2171b82ed218f
furyohfury <139155361+furyohfury@users.noreply.github.com>
2024-09-19 23:24:56 +0500

    chore/time manager

---
+++ Assets/Scripts/EnterExitTimeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace RealTime
+{
+	public class EnterExitTimeController : MonoBehaviour
+	{
+		
+		
+		private struct EnterQuitTime
+		{
+			public DateTime EnterTime;
+			public DateTime QuitTime;
+			public TimeSpan SessionDuration;
+		}
+	}
+}
---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+using Newtonsoft.Json;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace RealTime
+{
+	public sealed class TimeManager : MonoBehaviour
+	{
+		[SerializeField]
+		private TMP_Text _time;
+
+		private const string ENTER_QUIT_TIME_PREFS = "ENTER_QUIT_TIME_PREFS";
+		private const string SERVER_PATH = "https://timeapi.io/api/time/current/zone?timeZone=Europe%2FMoscow";
+
+		private async void Awake()
+		{
+			var time = await GetServerTime();
+			_time.text = time.ToString();
+		}
+
+		private async UniTask<DateTime> GetServerTime()
+		{
+			var request = UnityWebRequest.Get(SERVER_PATH);
+			await request.SendWebRequest();
+			if (request.result == UnityWebRequest.Result.Success)
+			{
+				var dateJson = request.downloadHandler.text;
+				var date = JsonConvert.DeserializeObject<ServerTimeData>(dateJson);
+				return DateTime.Parse(date.dateTime);
+			}
+			return default;
+		}
+
+		private struct ServerTimeData
+		{
+			public string dateTime;
+		}
+	}
+}
