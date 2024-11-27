f1626088c8bcddd37ccaa9fea98cf7fffd17eea5
furyohfury <139155361+furyohfury@users.noreply.github.com>
2024-11-27 12:14:08 +0500

    feat/lookatx util

--- Assets/Game/Common/Utils/VectorUtils.cs
+++ Assets/Game/Common/Utils/VectorUtils.cs
@@ -1,7 +1,14 @@
-﻿namespace Game.Common.Utils
+﻿namespace Game
 {
-	public class VectorUtils
+	public static class VectorUtils
 	{
-		
+		public static void LookAtX(this Transform transform, Vector3 targetPos)
+		{
+			var dir = targetPos - transform.position;
+			var angle = Vector3.Angle(transform.right, dir);
+			var rotation = Quaternion.Euler(new Vector3(angle, 0, 0));
+
+			transform.rotation *= rotation;
+		}
 	}
 }
