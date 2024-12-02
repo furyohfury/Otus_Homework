78caa902deede33df6e0daabd345f6aca604644b
furyohfury <139155361+furyohfury@users.noreply.github.com>
2024-12-02 16:58:30 +0500

    refac/shoot bh with pool

--- Assets/Game/Character/Combat/Scripts/PistolShootBehaviour.cs
+++ Assets/Game/Character/Combat/Scripts/PistolShootBehaviour.cs
@@ -7,28 +7,55 @@ namespace Game
 	public sealed class PistolShootBehaviour : IEntityInit, IEntityDispose
 	{
 		private BaseEvent _attackEvent;
-		private IValue<GameObject> _pistolBulletPrefab;
+		private IValue<SceneEntity> _pistolBulletPrefab;
 		private IValue<Transform> _firePoint;
+		private IValue<GameObject> _weapon;
+		private Pool<SceneEntity> _pool;
 
 		public void Init(IEntity entity)
 		{
 			_pistolBulletPrefab = entity.GetProjectilePrefab();
 			_firePoint = entity.GetFirePoint();
 			_attackEvent = entity.GetAttackEvent();
+			_weapon = entity.GetWeapon();
 			_attackEvent.Subscribe(OnAttackEvent);
+
+			_pool = new(_weapon, _pistolBulletPrefab.Value, true);
 		}
 
 		private void OnAttackEvent()
 		{
-			// TODO use pool with world?
-			
-			//can make pool HERE lol
-			GameObject.Instantiate(_pistolBulletPrefab.Value, _firePoint.Value.position, _firePoint.Value.rotation);
+			// TODO how to get world transform?
+			var bullet = _pool.Get(_weapon.transform, _firePoint.Value.position, _firePoint.Value.rotation);
+			if (bullet.TryGetDeathEvent(out BaseEvent deathEvent))
+			{
+				deathEvent.Subscribe(ReturnToPool);
+				// TODO make deathevent BaseEvent<SceneEntity>
+			}
+			else
+			{
+				Debug.LogError("Bullet has no death event");
+			}			
+		}
+
+		private void ReturnToPool(SceneEntity entity)
+		{
+			_pool.Return(entity);
 		}
 
 		public void Dispose(IEntity entity)
 		{
 			_attackEvent.Unsubscribe(OnAttackEvent);
+			foreach (var bullet in _pool.ActiveItems)
+			{
+				if (bullet.TryGetDeathEvent(out BaseEvent deathEvent))
+				{
+					deathEvent.Unsubscribe(ReturnToPool);
+					bullet.AddBehaviour<DestroyGameObjectOnDeathBehaviour>();
+				}
+			}
+
+			_pool.Dispose();
 		}
 	}
 }
--- Assets/Game/Common/Pool.cs
+++ Assets/Game/Common/Pool.cs
@@ -1,38 +1,95 @@
-﻿// using System;
-// using UnityEngine;
-// using UnityEngine.Pool;
-// using Object = UnityEngine.Object;
-//
-// namespace Game
-// {
-// 	public class Pool<T> where T : Object 
-// 	{
-// 		private Transform _container;
-// 		private T _prefab;
-// 		private ObjectPool<T> _pool;
-//
-// 		public Pool(Transform container, T prefab, int capacity = 10, int maxSize = 20)
-// 		{
-// 			var cache = new GameObject
-// 			            {
-// 				            transform =
-// 				            {
-// 					            parent = container
-// 				            }
-// 			            };
-// 			_container = cache.transform;
-// 			_prefab = prefab;
-// 			_pool = new ObjectPool<T>(CreateObject, GetObject, ReleaseObject, DestroyObject, false, capacity, maxSize);
-// 		}
-//
-// 		private void GetObject(T obj)
-// 		{
-// 			
-// 		}
-//
-// 		private T CreateObject()
-// 		{
-// 			return Object.Instantiate(_prefab, _container);
-// 		}
-// 	}
-// }
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+    public class Pool<T> where T : Component
+    {
+        public IReadonlyCollection ActiveItems => _activeItems;
+
+        protected Transform _container;
+        protected T _prefab;
+        protected int _initialSize;
+        protected int _maxSize;
+
+        protected readonly Queue<T> _inactiveItems;
+        protected readonly HashSet<T> _activeItems;
+
+        public Pool(Transform parent, T prefab, bool fillOnCreate, int initialSize = 10, int maxSize = 20)
+        {
+            _initialSize = initialSize;
+            _maxSize = maxSize;
+            _prefab = prefab;
+            _inactiveItems = new Queue(initialSize);
+            _container = new GameObject($"{prefab.name}Pool").transform;
+            _container.SetParent(parent);
+            _container.gameObject.SetActive(false);
+            
+            if (fillOnCreate)
+            {
+                FillPool();
+            }
+        }
+
+        public virtual T Get(Transform parent, Vector2 pos, Quaternion rot)
+        {
+            T item;
+            if (_inactiveItems.TryDequeue(out T item))
+            {
+                var transform = item.transform;
+                transform.parent = parent;
+                transform.position = pos;
+                transform.rotation = rot;
+                return item;
+            }
+            else
+            {
+                item = CreateItem(parent, pos, rot);
+            }
+
+            if (_activeItems.Add(item) == false)
+            {
+                Debug.LogError($"Item from pool is already active");
+            }
+        }
+
+        public virtual void Return(T item)
+        {
+            if (_activeItems.Remove(item) == false)
+            {
+                Debug.LogError($"Item is already inactive");
+            }
+
+            if (_inactiveItems.Count < _maxSize)
+            {
+                _inactiveItems.Enqueue(item);
+                item.transform.SetParent(_container);
+            }
+            else
+            {
+                Object.Destroy(item);
+            }
+            
+        }
+
+        protected T CreateItem(Transform parent, Vector2 pos, Quaternion rot)
+        {
+            return Object.Instantiate(_prefab, parent, pos, rot);
+        }
+
+        protected void FillPool(int count)
+        {
+            var pos = _container.transform.position;
+            for (var i = 0; i < count; i++)
+            {
+                var newItem = CreateItem(_container, pos, Quaternion.identity);
+                _inactiveItems.Enqueue(newItem);
+            }
+        }
+
+        public void Dispose()
+        {
+            Object.Destroy(_container.gameObject);
+        }
+    }
+}
