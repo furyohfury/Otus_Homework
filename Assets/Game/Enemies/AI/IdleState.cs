fa06d6681af2c882a23cdebbf284537bf6ddcde5
FuryOhFury <needsomespaceforit2@gmail.com>
2025-02-16 23:37:35 +0500

    chore/level service start

---
+++ Assets/Game/Enemies/AI/IdleState.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Atomic.AI;
+using Atomic.Entities;
+using UnityEngine;
+
+namespace Game
+{
+	[Serializable]
+	public sealed class IdleState : IState
+	{
+		public string Name => "IdleState";
+
+		public void OnEnter(IBlackboard blackboard)
+		{
+			var self = blackboard.GetEntity();
+			self.GetMoveDirection().Value = Vector2.zero;
+		}
+
+		public void OnUpdate(IBlackboard blackboard, float deltaTime)
+		{
+			
+		}
+
+		public void OnExit(IBlackboard blackboard)
+		{
+			
+		}
+	}
+}
---
+++ Assets/Game/Level/Scripts/LevelData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Game
+{
+	public sealed class LevelData
+	{
+		public string Name;
+		public Dictionary<Cups, List<TimeSpan>> CupsTargetTimes;
+		public List<TimeSpan> Results;
+	}
+}
