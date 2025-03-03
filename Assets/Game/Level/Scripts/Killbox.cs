86a172c28082d6d3f79dd52697f1ad3f11bf26d7
FuryOhFury <needsomespaceforit2@gmail.com>
2025-03-04 00:38:23 +0500

    feat/add killbox

---
+++ Assets/Game/Level/Scripts/Killbox.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Atomic.Entities;
+using UnityEngine;
+
+namespace Game
+{
+	[RequireComponent(typeof(Collider2D))]
+	public sealed class Killbox : MonoBehaviour
+	{
+		[SerializeField]
+		private Collider2D _collider2D;
+
+		private void Awake()
+		{
+			_collider2D.isTrigger = true;
+		}
+
+		private void OnTriggerEnter2D(Collider2D other)
+		{
+			if (other.TryGetEntity(out var entity)
+			    && entity.TryGetHealth(out var health))
+			{
+				health.Value = 0;
+			}
+		}
+	}
+}
