1d0489c0184efeca1006b8deb10fab7a1a2c783c
FuryOhFury <needsomespaceforit2@gmail.com>
2025-03-04 00:38:10 +0500

    chore/add enemy prefabs config for saveload

---
+++ Assets/SaveLoad/SaveLoaders/DataStorages/Enemies/EnemyData.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace SaveLoad
+{
+	public sealed class EnemyData
+	{
+		public int InstanceId;
+		public string Id;
+		public Vector3 Position;
+		public Quaternion Rotation;
+		public int Health;
+
+		public EnemyData(int instanceId, string id, Vector3 position, Quaternion rotation, int health)
+		{
+			InstanceId = instanceId;
+			Id = id;
+			Position = position;
+			Rotation = rotation;
+			Health = health;
+		}
+	}
+}
---
+++ Assets/SaveLoad/SaveLoaders/DataStorages/Enemies/EnemyPrefabs.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Atomic.Entities;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace SaveLoad
+{
+	[CreateAssetMenu(fileName = "EnemyPrefabs", menuName = "Create saveload config/Enemy prefabs config")]
+	public sealed class EnemyPrefabs : SerializedScriptableObject
+	{
+		public Dictionary<string, SceneEntity> Prefabs => _enemyPrefabs;
+
+		[SerializeField]
+		private Dictionary<string, SceneEntity> _enemyPrefabs;
+	}
+}
