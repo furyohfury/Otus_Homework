f936900e7d744575e16b90af89f231fe95a74f7e
FuryOhFury <needsomespaceforit2@gmail.com>
2025-03-04 00:34:47 +0500

    refac/separate character death observer on ui and usual

---
+++ Assets/Game/Level/Scripts/CharacterDeathObserver.cs
@@ -0,0 +1,41 @@
+﻿using Atomic.Elements;
+using Atomic.Entities;
+using Game;
+using UnityEngine;
+using Zenject;
+
+namespace Game
+{
+	public sealed class CharacterDeathObserver : IInitializable, IGameTickable
+	{
+		private readonly IEntity _character;
+		private IFunction<bool> _playerIsDead;
+		private readonly GameStateManager _gameStateManager;
+
+		[Inject]
+		public CharacterDeathObserver(IEntity character, GameStateManager gameStateManager)
+		{
+			_character = character;
+			_gameStateManager = gameStateManager;
+		}
+
+		public void Initialize()
+		{
+			if (_character.TryGetIsDead(out BaseFunction<bool> isDead) == false)
+			{
+				Debug.LogError("Cant find isDead on player");
+				return;
+			}
+
+			_playerIsDead = isDead;
+		}
+
+		public void Tick(float deltaTime)
+		{
+			if (_playerIsDead.Invoke())
+			{
+				_gameStateManager.ChangeState(GameState.Pause);
+			}
+		}
+	}
+}
---
+++ Assets/UI/GameInterface/Level/CharacterDeathUIObserver.cs
@@ -0,0 +1,42 @@
+﻿using Atomic.Elements;
+using Atomic.Entities;
+using Game;
+using UnityEngine;
+using Zenject;
+
+namespace UI
+{
+	public sealed class CharacterDeathUIObserver : IInitializable, IGameTickable
+	{
+		private readonly IEntity _character;
+		private IFunction<bool> _playerIsDead;
+		private readonly GameOverMenuView _gameOverMenuView;
+
+		[Inject]
+		public CharacterDeathUIObserver(IEntity character, GameOverMenuView gameOverMenuView)
+		{
+			_character = character;
+			_gameOverMenuView = gameOverMenuView;
+		}
+
+
+		public void Initialize()
+		{
+			if (_character.TryGetIsDead(out BaseFunction<bool> isDead) == false)
+			{
+				Debug.LogError("Cant find isDead on player");
+				return;
+			}
+
+			_playerIsDead = isDead;
+		}
+
+		public void Tick(float deltaTime)
+		{
+			if (_playerIsDead.Invoke())
+			{
+				_gameOverMenuView.gameObject.SetActive(true);
+			}
+		}
+	}
+}
