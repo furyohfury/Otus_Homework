7ce1fc520e7c16846258fd93deeb113049453c69
FuryOhFury <needsomespaceforit2@gmail.com>
2025-03-08 12:42:33 +0500

    feat/add game repo debug helper

---
+++ Assets/Debug/GameRepositoryDebugHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using SaveLoad;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using Zenject;
+
+namespace GameDebug
+{
+	public class GameRepositoryDebugHelper : MonoBehaviour
+	{
+		[ShowInInspector]
+		public IReadOnlyDictionary<string, string> gamestate => _gameRepository.GameState;
+		
+		private GameRepository _gameRepository;
+
+		private void Awake()
+		{
+			var projectContext = FindObjectOfType<ProjectContext>();
+			_gameRepository = projectContext.Container.Resolve<GameRepository>();
+		}
+	}
+}
