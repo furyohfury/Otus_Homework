128c454a28ccbbec8e741889333b14e1bc6e119f
FuryOhFury <needsomespaceforit2@gmail.com>
2025-03-08 12:44:55 +0500

    feat/add launch activated saveloadercontroller

---
+++ Assets/Zenject/Saveload/Installers/Scripts/LaunchSaveLoadersController.cs
@@ -0,0 +1,20 @@
+﻿using Zenject;
+
+namespace SaveLoad
+{
+	public sealed class LaunchSaveLoadersController : IInitializable
+	{
+		private readonly SaveLoadManager _saveLoadManager;
+
+		[Inject]
+		public LaunchSaveLoadersController(SaveLoadManager saveLoadManager)
+		{
+			_saveLoadManager = saveLoadManager;
+		}
+
+		public void Initialize()
+		{
+			_saveLoadManager.LoadSpecific<LevelResultsSaveLoader>();
+		}
+	}
+}
