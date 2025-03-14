1129fa6319f92c410c7ac2dc80cff967265fffc1
FuryOhFury <needsomespaceforit2@gmail.com>
2025-03-14 20:08:25 +0500

    feat/add player service

---
+++ Assets/Game/Services/PlayerService.cs
@@ -0,0 +1,18 @@
+﻿using Atomic.Entities;
+using Zenject;
+
+namespace Game
+{
+	public sealed class PlayerService
+	{
+		public IEntity Player => _player;
+
+		private readonly SceneEntity _player;
+
+		[Inject]
+		public PlayerService(SceneEntity player)
+		{
+			_player = player;
+		}
+	}
+}
