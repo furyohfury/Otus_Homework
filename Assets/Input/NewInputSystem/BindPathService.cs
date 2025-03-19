d029288e9a20b92b4ce2830458250b2d9dea49ea
FuryOhFury <needsomespaceforit2@gmail.com>
2025-03-19 21:04:34 +0500

    feat/add bind path service

---
+++ Assets/Input/NewInputSystem/BindPathService.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine.InputSystem;
+
+namespace Game
+{
+	public sealed class BindPathService
+	{
+		private readonly InputControls _inputControls;
+
+		public BindPathService(InputControls inputControls)
+		{
+			_inputControls = inputControls;
+		}
+
+		public string GetPath(string action, string scheme = null)
+		{
+			var inputAction = _inputControls.FindAction(action);
+			if (scheme == null)
+			{
+				return inputAction.bindings[0].path;
+			}
+
+			int bindingIndex = 0;
+			bindingIndex = inputAction.bindings.IndexOf(b =>
+				b.groups != null
+				&& b.groups.Contains(scheme));
+
+			return bindingIndex != -1
+				? inputAction.bindings[bindingIndex].effectivePath
+				: null;
+		}
+
+		public string GetFormattedPath(string action, string scheme)
+		{
+			return InputControlPath.ToHumanReadableString(GetPath(action, scheme), InputControlPath.HumanReadableStringOptions.UseShortNames);
+		}
+	}
+}
