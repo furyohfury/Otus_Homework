567f76d36bc0ab635f9765ea86a5d63d13f62c9f
FuryOhFury <needsomespaceforit2@gmail.com>
2025-03-23 13:54:18 +0500

    feat/helper for rebind removal in editor

---
+++ Assets/Input/NewInputSystem/Helpers/PlayerPrefsRebindHelper.cs
@@ -0,0 +1,25 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Game
+{
+	[CreateAssetMenu(fileName = "PlayerPrefsRebindHelper", menuName = "Create helper/PlayerPrefsRebindHelper")]
+	public sealed class PlayerPrefsRebindHelper : ScriptableObject
+	{
+		[SerializeField]
+		private string _key;
+
+		[Button]
+		private void ClearRebindPrefs()
+		{
+			if (PlayerPrefs.HasKey(_key))
+			{
+				PlayerPrefs.DeleteKey(_key);
+			}
+			else
+			{
+				Debug.Log($"No key: {_key} in player prefs");
+			}
+		}
+	}
+}
