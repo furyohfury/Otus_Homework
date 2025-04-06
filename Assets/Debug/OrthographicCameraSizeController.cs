ea9edf5656dbb80349b3121e527b0bbe30c0f11f
FuryOhFury <needsomespaceforit2@gmail.com>
2025-04-06 23:41:28 +0500

    chore/physics materials tweaking

---
+++ Assets/Debug/OrthographicCameraSizeController.cs
@@ -0,0 +1,17 @@
+﻿using Cinemachine;
+using UnityEngine;
+
+namespace GameDebug
+{
+	public class OrthographicCameraSizeController : MonoBehaviour
+	{
+		[SerializeField]
+		private CinemachineVirtualCamera _camera;
+		private const float PIXELS_TO_WORLD_UNIT = 200f;
+
+		private void Awake()
+		{
+			_camera.m_Lens.OrthographicSize = Screen.height / 2f / PIXELS_TO_WORLD_UNIT;
+		}
+	}
+}
