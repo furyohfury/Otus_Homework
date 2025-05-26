95198404c1794f1902fca1f9689acebebfdc1b6c
FuryOhFury <needsomespaceforit2@gmail.com>
2025-05-26 22:53:40 +0500

    feat/make flipSpriteHelper

---
+++ Assets/Debug/FlipSpriteHelper.cs
@@ -0,0 +1,21 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace GameDebug
+{
+	public class FlipSpriteHelper : MonoBehaviour
+	{
+		[SerializeField]
+		private GameObject _target;
+
+		[Button]
+		private void  FlipAllX(bool flip)
+		{
+			var renderers = GetComponentsInChildren<SpriteRenderer>();
+			for (int i = 0, count = renderers.Length; i < count; i++)
+			{
+				renderers[i].flipX = flip;
+			}
+		}
+	}
+}
