dbfb25d3f30cd0cf8d11727a68540519778d1ca0
FuryOhFury <needsomespaceforit2@gmail.com>
2025-06-19 13:26:17 +0500

    feat/add DestroyAnimationComponent

---
+++ Assets/Game/UnityEventsReceivers/DestroyAnimationComponent.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+	public sealed class DestroyAnimationComponent : MonoBehaviour
+	{
+		public void DestroyObject()
+		{
+			Destroy(gameObject);
+		}
+	}
+}
