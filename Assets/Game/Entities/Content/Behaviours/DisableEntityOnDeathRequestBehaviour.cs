25ede4631dbca6c2270bae0aa1b5e15a74e8650e
FuryOhFury <needsomespaceforit2@gmail.com>
2025-06-20 20:05:42 +0500

    feat/add DisableEntityOnDeathRequestBehaviour

---
+++ Assets/Game/Entities/Content/Behaviours/DisableEntityOnDeathRequestBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using Atomic.Elements;
+using Atomic.Entities;
+
+namespace Game
+{
+	public sealed class DisableEntityOnDeathRequestBehaviour : IEntityInit, IEntityDispose
+	{
+		private IEvent _deathEvent;
+		private IEntity _entity;
+
+		public void Init(IEntity entity)
+		{
+			_entity = entity;
+			_deathEvent = entity.GetDeathEvent();
+			_deathEvent.Subscribe(OnDeathRequest);
+		}
+
+		private void OnDeathRequest()
+		{
+			_entity.Disable();
+		}
+
+		public void Dispose(IEntity entity)
+		{
+			_deathEvent.Unsubscribe(OnDeathRequest);
+		}
+	}
+}
