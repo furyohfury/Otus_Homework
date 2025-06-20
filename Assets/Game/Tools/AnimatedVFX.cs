534fbc3dda51868a4336f07202265cdb8b661cb2
FuryOhFury <needsomespaceforit2@gmail.com>
2025-06-20 18:01:53 +0500

    style/receivers rename to tools

---
+++ Assets/Game/Tools/AnimatedVFX.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game
+{
+	public sealed class AnimatedVFX : MonoBehaviour
+	{
+		public event Action OnEnded; 
+		
+		public void AnimationEnded()
+		{
+			OnEnded?.Invoke();
+			Destroy(gameObject);
+		}
+	}
+}
