3a6618c97b19107fdda280345852a5956c83f1ee
FuryOhFury <needsomespaceforit2@gmail.com>
2025-06-20 18:01:17 +0500

    debug/add DeathVFXDebug

---
+++ Assets/Debug/DeathVFXDebug.cs
@@ -0,0 +1,47 @@
+﻿using DG.Tweening;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace GameDebug
+{
+	public class DeathVFXDebug : MonoBehaviour
+	{
+		[SerializeField]
+		private SpriteRenderer _spriteRenderer;
+		[SerializeField]
+		private float _dissolveDuration = 0.5f;
+		[SerializeField]
+		private GameObject _explodePrefab;
+
+		[Button]
+		private void SetFade(float f)
+		{
+			_spriteRenderer.material.SetFloat("Vector1_51DDBE76", f);
+		}
+		
+		[Button]
+		private void DissolveDeath()
+		{
+			_spriteRenderer.sharedMaterial.DOFloat(0, "Vector1_51DDBE76", _dissolveDuration);
+		}
+
+		[Button]
+		private void ResetDissolve()
+		{
+			_spriteRenderer.sharedMaterial.SetFloat("Vector1_51DDBE76", 1);
+		}
+
+		[Button]
+		private void ExplodeDeath()
+		{
+			_spriteRenderer.enabled = false;
+			Instantiate(_explodePrefab, _spriteRenderer.transform.position, Quaternion.identity);
+		}
+		
+		[Button]
+		private void ResetExplode()
+		{
+			_spriteRenderer.enabled = true;
+		}
+	}
+}
