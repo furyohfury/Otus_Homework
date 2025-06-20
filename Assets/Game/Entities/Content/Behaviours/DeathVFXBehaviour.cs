803d71b20923f282685c2b7bdc8b4b2714340f53
FuryOhFury <needsomespaceforit2@gmail.com>
2025-06-20 13:14:13 +0500

    chore/move bhs to content folder

---
+++ Assets/Game/Entities/Content/Behaviours/DeathVFXBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Atomic.Entities;
+using UnityEngine;
+
+namespace Game
+{
+	public sealed class DeathVFXBehaviour : IEntityInit, IEntityDispose
+	{
+		[SerializeField]
+		private GameObject _vfx;
+		
+		public void Init(IEntity entity)
+		{
+			if (entity.TryGetDeathEvent(out var deathEvent))
+			{
+				deathEvent.Subscribe(OnDeath);
+			}
+		}
+
+		private void OnDeath()
+		{
+			
+		}
+
+		public void Dispose(IEntity entity)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
