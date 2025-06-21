d26aebd33b9073d2875809f18bb8c705d6926fd8
FuryOhFury <needsomespaceforit2@gmail.com>
2025-06-21 18:34:41 +0500

    debug/add SpriteMaterialChangeDebugHelper

---
+++ Assets/Debug/SpriteMaterialChangeDebugHelper.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace GameDebug
+{
+	public sealed class SpriteMaterialChangeDebugHelper : MonoBehaviour
+	{
+		[SerializeField]
+		private SpriteRenderer[] _spriteRenderers;
+		[SerializeField]
+		private Material _defaultMaterial;
+		[SerializeField]
+		private Material _litMaterial;
+
+		[Button]
+		private void CollectSpriteRenderers()
+		{
+			_spriteRenderers = FindObjectsByType<SpriteRenderer>(FindObjectsSortMode.None)
+			                   .Where(spriteRenderer => spriteRenderer.sharedMaterial == _defaultMaterial)
+			                   .ToArray();
+		}
+		
+		[Button]
+		private void ChangeMaterialToSpriteRenderers()
+		{
+			for (int i = 0, count = _spriteRenderers.Length; i < count; i++)
+			{
+				_spriteRenderers[i].sharedMaterial = _litMaterial;
+			}
+		}
+	}
+}
