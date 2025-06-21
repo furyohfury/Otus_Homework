c4abc4516d70b35e8154c7f1ede85545e775189f
FuryOhFury <needsomespaceforit2@gmail.com>
2025-06-21 13:53:51 +0500

    debug/add ability and weapon debug

---
+++ Assets/Debug/AbilityEquipDebugHelper.cs
@@ -0,0 +1,22 @@
+﻿using Atomic.Entities;
+using Game;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace GameDebug
+{
+	public sealed class AbilityEquipDebugHelper : MonoBehaviour
+	{
+		[SerializeField]
+		private AbilityCardConfig _config;
+		[SerializeField]
+		private SceneEntity _player;
+
+		[Button]
+		private void EquipWeapon()
+		{
+			_player.GetRemoveActiveAbilityEvent().Invoke();
+			_player.GetAbilityCardPickupEvent().Invoke(_config);
+		}
+	}
+}
---
+++ Assets/Debug/WeaponEquipDebugHelper.cs
@@ -0,0 +1,21 @@
+﻿using Atomic.Entities;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace GameDebug
+{
+	public sealed class WeaponEquipDebugHelper : MonoBehaviour
+	{
+		[SerializeField]
+		private SceneEntity _prefab;
+		[SerializeField]
+		private SceneEntity _player;
+
+		[Button]
+		private void EquipWeapon()
+		{
+			_player.GetUnequipWeaponRequest().Invoke();
+			_player.GetEquipWeaponRequest().Invoke(_prefab);
+		}
+	}
+}
