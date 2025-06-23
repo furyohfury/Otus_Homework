a3ad326d88207f0f0c88771350b7f2949bd20e1d
FuryOhFury <needsomespaceforit2@gmail.com>
2025-06-23 22:48:24 +0500

    feat/add AttackOnPositionState to enemy AI

---
+++ Assets/Game/Entities/Content/Enemies/AI/AttackOnPositionState.cs
@@ -0,0 +1,42 @@
+﻿using Atomic.AI;
+using Atomic.Elements;
+using Atomic.Entities;
+using UnityEngine;
+
+namespace Game
+{
+	public sealed class AttackOnPositionState : IState
+	{
+		public string Name => "AttackOnPositionState";
+
+		private IEvent _attackRequest;
+		private SceneEntity _self;
+		private float _attackRange;
+
+		public void OnEnter(IBlackboard blackboard)
+		{
+			if (blackboard.TryGetEntity(out var self))
+			{
+				_attackRequest = self.GetAttackRequest();
+				_self = self;
+			}
+
+			_attackRange = blackboard.GetAttackRange();
+		}
+
+		public void OnUpdate(IBlackboard blackboard, float deltaTime)
+		{
+			var target = blackboard.GetTarget();
+			var direction = target.transform.position - _self.transform.position;
+			var targetInRange = direction.sqrMagnitude <= _attackRange * _attackRange;
+			if (targetInRange)
+			{
+				_attackRequest.Invoke();
+			}
+		}
+
+		public void OnExit(IBlackboard blackboard)
+		{
+		}
+	}
+}
