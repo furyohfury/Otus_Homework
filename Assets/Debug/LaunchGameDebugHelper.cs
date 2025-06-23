2e62c0ea778a57c88ddca9b0fb5b14985cf4ce8b
FuryOhFury <needsomespaceforit2@gmail.com>
2025-06-23 22:45:46 +0500

    debug/add LaunchGameDebugHelper

---
+++ Assets/Debug/LaunchGameDebugHelper.cs
@@ -0,0 +1,28 @@
+﻿using Game;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Zenject;
+
+namespace GameDebug
+{
+	public sealed class LaunchGameDebugHelper : MonoBehaviour
+	{
+#if UNITY_EDITOR
+		private LevelManager _levelManager;
+
+		[Inject]
+		public void Construct(LevelManager levelManager)
+		{
+			_levelManager = levelManager;
+		}
+
+		private void Update()
+		{
+			if (Keyboard.current.gKey.wasPressedThisFrame)
+			{
+				_levelManager.StartLevel();
+			}
+		}
+#endif
+	}
+}
