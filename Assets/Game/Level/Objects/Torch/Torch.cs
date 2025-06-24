ca144ec68a5567bb85372f27a6cfac87da2ad6b5
FuryOhFury <needsomespaceforit2@gmail.com>
2025-06-24 12:33:34 +0500

    style/move leve objects folder

---
+++ Assets/Game/Level/Objects/Torch/Torch.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+namespace Game
+{
+	public sealed class Torch : MonoBehaviour
+	{
+		[SerializeField]
+		private Light2D _light2D;
+		[SerializeField]
+		private float _frequency;
+		[SerializeField]
+		private AnimationCurve _curve;
+
+		private void UpdateLight()
+		{
+			
+		}
+	}
+}
