c92628d58722f073a871984b904da12ce2d47ff5
FuryOhFury <needsomespaceforit2@gmail.com>
2025-06-24 21:48:13 +0500

    fix/editor only objects bullshit fix

---
+++ Assets/Debug/FuckingEditorOnlyTagHelper.cs
@@ -0,0 +1,18 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace GameDebug
+{
+	public sealed class FuckingEditorOnlyTagHelper : MonoBehaviour
+	{
+		[Button]
+		private void DoShit()
+		{
+			GameObject[] editorOnlyGameObjects = GameObject.FindGameObjectsWithTag("EditorOnly");
+			for (int i = 0, count = editorOnlyGameObjects.Length; i < count; i++)
+			{
+				editorOnlyGameObjects[i].tag = "Untagged";
+			}
+		}
+	}
+}
