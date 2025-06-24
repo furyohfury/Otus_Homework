209e499a95a7d58cca578feeb7ded93131512b29
FuryOhFury <needsomespaceforit2@gmail.com>
2025-06-24 13:17:51 +0500

    chore/add light flicker to torch

--- Assets/Game/Level/Objects/Torch/Torch.cs
+++ Assets/Game/Level/Objects/Torch/Torch.cs
@@ -1,20 +1,45 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.Rendering.Universal;
 
 namespace Game
 {
 	public sealed class Torch : MonoBehaviour
 	{
-		[SerializeField]
-		private Light2D _light2D;
-		[SerializeField]
-		private float _frequency;
-		[SerializeField]
-		private AnimationCurve _curve;
+		public AnimationCurve intensityCurve = AnimationCurve.Linear(0, 1, 1, 1);
+		public float duration = 1f;
+		public bool loop = true;
 
-		private void UpdateLight()
+		[SerializeField]
+		private Light2D light2D;
+		private float time;
+		private float baseIntensity;
+
+		void Start()
 		{
-			
+			baseIntensity = light2D.intensity;
+			time = 0f;
+		}
+
+		void Update()
+		{
+			if (duration <= 0f) return;
+
+			time += Time.deltaTime;
+
+			float t = time / duration;
+
+			if (loop)
+			{
+				t %= 1f;
+			}
+			else if (t > 1f)
+			{
+				t = 1f;
+			}
+
+			float curveValue = intensityCurve.Evaluate(t);
+			light2D.intensity = baseIntensity * curveValue;
 		}
 	}
 }
