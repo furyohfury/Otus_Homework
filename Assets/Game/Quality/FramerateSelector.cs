0e469aacf7a549ed5d7043d808df908af325e310
FuryOhFury <needsomespaceforit2@gmail.com>
2025-06-26 13:15:45 +0500

    feat/add FramerateSelector

---
+++ Assets/Game/Quality/FramerateSelector.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using Zenject;
+
+namespace Game
+{
+	public sealed class FramerateSelector : IInitializable, ITickable
+	{
+		public void Initialize()
+		{
+			Application.targetFrameRate = 30;
+		}
+
+		public void Tick()
+		{
+			if (Keyboard.current.numpad0Key.wasPressedThisFrame)
+			{
+				QualitySettings.SetQualityLevel(0);
+			}
+			else if (Keyboard.current.numpad1Key.wasPressedThisFrame)
+			{
+				QualitySettings.SetQualityLevel(1);
+			}
+			else if (Keyboard.current.numpad2Key.wasPressedThisFrame)
+			{
+				QualitySettings.SetQualityLevel(2);
+			}
+		}
+	}
+}
---
+++ Assets/Game/Zenject/Quality/QualitySystemsInstaller.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace Game
+{
+	[CreateAssetMenu(fileName = "QualitySystemsInstaller", menuName = "Create installer/Quality/QualitySystemsInstaller")]
+	public sealed class QualitySystemsInstaller : ScriptableObjectInstaller
+	{
+		public override void InstallBindings()
+		{
+#if UNITY_WEBGL
+			Container.BindInterfacesAndSelfTo<FramerateSelector>()
+			         .AsSingle();
+#endif
+		}
+	}
+}
