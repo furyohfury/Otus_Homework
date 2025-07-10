16e10b58225f2f14345615a05e5172d4f686fdf4
FuryOhFury <needsomespaceforit2@gmail.com>
2025-07-10 20:47:21 +0500

    refac/add trycatch to gamerepository

--- Assets/Game/Modules/SaveLoad/GameRepository/GameRepository.cs
+++ Assets/Game/Modules/SaveLoad/GameRepository/GameRepository.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using Newtonsoft.Json;
 using UnityEngine;
@@ -9,8 +10,8 @@ namespace SaveLoad
 	{
 		public IReadOnlyDictionary<string, string> GameState => _gameState;
 
-		private const string SAVE_FILE_NAME = "SaveFile.txt";
-		private static string SaveFilePath => string.Concat(Application.persistentDataPath, "/", SAVE_FILE_NAME);
+		private const string SAVE_FILE_NAME = "savefile.json";
+		private static string SaveFilePath => Path.Combine(Application.persistentDataPath, SAVE_FILE_NAME);
 		private Dictionary<string, string> _gameState = new();
 
 		public void LoadState()
@@ -18,12 +19,19 @@ namespace SaveLoad
 			if (File.Exists(SaveFilePath))
 			{
 				var savedState = File.ReadAllText(SaveFilePath);
-				_gameState = JsonConvert.DeserializeObject<Dictionary<string, string>>(savedState, new JsonSerializerSettings
-				                                                                                   {
-					                                                                                   ReferenceLoopHandling =
-						                                                                                   ReferenceLoopHandling.Ignore
-					                                                                                   , TypeNameHandling = TypeNameHandling.Objects
-				                                                                                   });
+				try
+				{
+					_gameState = JsonConvert.DeserializeObject<Dictionary<string, string>>(savedState, new JsonSerializerSettings
+						{
+							ReferenceLoopHandling =
+								ReferenceLoopHandling.Ignore
+							, TypeNameHandling = TypeNameHandling.Objects
+						});
+				}
+				catch (Exception e)
+				{
+					Debug.LogError($"Deserialize error in LoadState().\nStack trace:{e.StackTrace}");
+				}
 			}
 			else
 			{
@@ -43,22 +51,38 @@ namespace SaveLoad
 
 		public T GetData<T>()
 		{
-			var serializedData = _gameState[typeof(T).FullName];
-			return JsonConvert.DeserializeObject<T>(serializedData, new JsonSerializerSettings
-			                                                        {
-				                                                        TypeNameHandling = TypeNameHandling.Objects
-			                                                        });
+			var serializedData = _gameState[typeof(T).FullName!];
+			try
+			{
+				return JsonConvert.DeserializeObject<T>(serializedData, new JsonSerializerSettings
+				                                                        {
+					                                                        TypeNameHandling = TypeNameHandling.Objects
+				                                                        });
+			}
+			catch (Exception e)
+			{
+				Debug.LogError($"Deserialize error in GetData() of type {typeof(T).FullName}.\nStack trace:{e.StackTrace}");
+				throw;
+			}
 		}
 
 		public bool TryGetData<T>(out T value)
 		{
 			if (_gameState.TryGetValue(typeof(T).FullName!, out var serializedData))
 			{
-				value = JsonConvert.DeserializeObject<T>(serializedData, new JsonSerializerSettings
-				                                                         {
-					                                                         TypeNameHandling = TypeNameHandling.Objects
-				                                                         });
-				return true;
+				try
+				{
+					value = JsonConvert.DeserializeObject<T>(serializedData, new JsonSerializerSettings
+					                                                         {
+						                                                         TypeNameHandling = TypeNameHandling.Objects
+					                                                         });
+					return true;
+				}
+				catch (Exception e)
+				{
+					Debug.LogError($"Deserialize error in TryGetData() of type {typeof(T).FullName}.\nStack trace:{e.StackTrace}");
+					throw;
+				}
 			}
 
 			value = default;
